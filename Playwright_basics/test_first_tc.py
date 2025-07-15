62ba90e007d9325d9353f08b4de471d71191bbd6
Ravindra Kadam <ravindrakdm22@gmail.com>
2025-07-15 20:33:14 +0530

    Updated test case test_first_tc.py

--- Playwright_basics/test_first_tc.py
+++ Playwright_basics/test_first_tc.py
@@ -6,20 +6,14 @@ import pytest
                                                 pytest.param("visual_user","secret_sauce",
                                                              marks=pytest.mark.skip("skipping for test purpose")),
                                                 pytest.param("locked_out_user","secret_sauce1", marks=pytest.mark.xfail)])
-def test_first_tc_scratch(playwright: Playwright, username, password) -> None:
-    browser = playwright.chromium.launch(headless=False, slow_mo=2000)
-    context = browser.new_context()
-
-    context.tracing.start(screenshots=True, sources=True, snapshots=True)
-
-    page = context.new_page()
+def test_first_tc_scratch(page, username, password) -> None:
     page.goto("http://www.saucedemo.com")
 
     page.locator("#user-name").fill(username)
     page.locator("#password").fill(password)
     page.locator("#login-button").click()
     expect(page.locator("//span[text()='Products']")).to_be_visible()
-    context.tracing.stop(path="trace_first_tc.zip")
+
 #
 #
 # with sync_playwright() as playwright:
